e56094d15e78b3c861e935cbc9307f5034e879be
HaydarJohn <haydarcan_2003@hotmail.com>
2023-04-11 14:41:53 +0300

    sea

--- src/Game.cpp
+++ src/Game.cpp
@@ -5,6 +5,7 @@ Game::Game()
     _window = nullptr;
     _screenWidth = 1024;
     _screenHeight = 720;
+    _gameState = GameState::PLAY;
 }
 
 Game::~Game()
@@ -22,4 +23,17 @@ void Game::initSystem()
     SDL_Init(SDL_INIT_EVERYTHING);
 
     _window = SDL_CreateWindow("Pass Out Day Project\0",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,_screenWidth,_screenHeight,SDL_WINDOW_OPENGL);
+}
+
+void Game::processInput()
+{
+
+}
+void Game::gameloop()
+{
+    while (_gameState != GameState::Exit)
+    {
+        processInput();
+    }
+    
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,11 @@
 #include<SDL2/SDL.h>
 #include<GL/glew.h>
 
-#include<iostream>
 
 int main(int argc,const char** argv)
 {
     Game game;
-
     game.run();
-
-    std::cin.get();
-
+    
     return 0;    
 }
