99743096698cb7090a9f608fdc04df43b85ceda4
haydarjohn <haydarcan_2003@hotmail.com>
2026-02-18 14:01:31 +0300

    Revert "simplify projects entry point"
    This reverts commit 651b1243f3db1e4e1bdcd20d033e48176acee267.

--- pong/src/main.cpp
+++ pong/src/main.cpp
@@ -2,7 +2,8 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <iostream>
-#include "engine.h"
+#include "window.h"
+
 
 
 
@@ -10,24 +11,28 @@ int main()
 {
     
     
-    Engine* engine = new Engine();
-
-    engine->run();
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,GL_TRUE);
+    #endif
     
-    
+    Window* window = new Window(1920,1080,"Pong");
 
 
     
 
-    // while (!glfwWindowShouldClose(window->getCurrentWindowContext()))
-    // {
+    while (!glfwWindowShouldClose(window->getCurrentWindowContext()))
+    {
         
 
 
-    //     glfwPollEvents();
-    //     glfwSwapBuffers(window->getCurrentWindowContext());
-    // }
+        glfwPollEvents();
+        glfwSwapBuffers(window->getCurrentWindowContext());
+    }
     
-    // glfwTerminate();
+    glfwTerminate();
     return 0;
 }
